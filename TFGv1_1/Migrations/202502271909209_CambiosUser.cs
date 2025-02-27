cfa23fbffacfff31e2dd73e0e257fa1c920810e5
delprado <i82gavaj@uco.es>
2025-02-27 20:10:50 +0100

    Migracion

---
+++ TFGv1_1/Migrations/202502271909209_CambiosUser.cs
@@ -0,0 +1,20 @@
+﻿namespace TFGv1_1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CambiosUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+            AddColumn("dbo.AspNetUsers", "Surname", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Surname");
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
